85f3e3494d5f943bbb92a6399e7fd0723755b5c7
ysr <141933879+jysr0@users.noreply.github.com>
2025-10-24 12:35:46 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */ //--------------------------------------------------
-static const unsigned int snap      = 12;       /* snap pixel */ //--------------------------------------------------
-static const int showbar            = 1;        /* 0 means no bar */ //--------------------------------------------------
-static const int topbar             = 1;        /* 0 means bottom bar */ //--------------------------------------------------
-static const char *fonts[]          = { "CaskaydiaMono Nerd Font Mono:size=10" }; //--------------------------------------------------
-static const char dmenufont[]       = "CaskaydiaMono Nerd Font Mono:size=10"; //--------------------------------------------------
+static const unsigned int borderpx  = 2;        /* border pixel of windows */   //---------------
+static const unsigned int snap      = 12;       /* snap pixel */                //---------------
+static const int showbar            = 1;        /* 0 means no bar */            //---------------
+static const int topbar             = 1;        /* 0 means bottom bar */        //---------------
+static const char *fonts[]          = {"CaskaydiaMono Nerd Font Mono:size=10"}; //---------------
+static const char dmenufont[]       = "CaskaydiaMono Nerd Font Mono:size=10";   //---------------
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577"; //--------------------------------------------------
+static const char col_cyan[]        = "#005577";                                //---------------
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -46,8 +46,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-/* Mod4Mask = (super/meta/windows) key, Mod1Mask = alt, ControlMask = ctrl, ShiftMask = shift */
-#define MODKEY Mod4Mask //--------------------------------------------------
+/* Mod4Mask = `super` or `meta` or `windows` key | Mod1Mask = alt | ControlMask = ctrl | ShiftMask = shift */
+#define MODKEY Mod4Mask //---------------
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -60,12 +60,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL }; //--------------------------------------------------
+static const char *termcmd[]  = { "alacritty", NULL }; //---------------
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } }, //--------------------------------------------------
-	/* { MODKEY,             XK_Return, spawn,          {.v = termcmd } }, */ //--------------------------------------------------
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },   //---------------
+	/* { MODKEY,                       XK_Return, spawn,          {.v = termcmd } }, */ //---------------
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -73,7 +73,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} }, //--------------------------------------------------
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },                //---------------
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,             		XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -105,17 +105,17 @@ static const Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} }, //--------------------------------------------------
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY|ShiftMask,         Button1,        resizemouse,    {0} }, //--------------------------------------------------
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	/* click                event mask         button          function        argument */
+	{ ClkLtSymbol,          0,                 Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,                 Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,                 Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,                 Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,            Button1,        movemouse,      {0} }, //---------------
+	{ ClkClientWin,         MODKEY,            Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask,  Button1,        resizemouse,    {0} }, //---------------
+	{ ClkTagBar,            0,                 Button1,        view,           {0} },
+	{ ClkTagBar,            0,                 Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,            Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,            Button3,        toggletag,      {0} },
 };
 
