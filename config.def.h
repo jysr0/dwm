e979f65c55f07787eb2fbfefef321b7ccb58bc5d
ysr <141933879+jysr0@users.noreply.github.com>
2026-01-29 03:48:27 +0100

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -33,14 +33,15 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       0,            0,           0,           -1 },
-	{ "mpv",      NULL,       NULL,       0,            1,           1,           -1 },
+    /* xprop(1):
+     *	WM_CLASS(STRING) = instance, class
+     *	WM_NAME(STRING) = title
+     */
+    /* class                 instance          title       tags mask     iscentered   isfloating   monitor */
+    { "Gimp",                NULL,             NULL,       0,            0,           1,           -1 },
+    { "Firefox",             NULL,             NULL,       0,            0,           0,           -1 },
+    { "mpv",                 NULL,             NULL,       0,            1,           1,           -1 },
+    { "Nsxiv",               NULL,             NULL,       0,            1,           1,           -1 },
 };
 
 /* layout(s) */
