ec98ab2fd1fecf07ba1498a65a18515683101d33
ysr <141933879+jysr0@users.noreply.github.com>
2026-02-04 15:02:22 +0100

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -92,7 +92,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "rofi", "-show", "drun" };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_cyan, "-sf", col_gray5, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 #include "shift-tools.c"
 #include "bulkill.c"
 #include "movestack.c"
