43062c6dbbfebad72b46ca13d6577c91847092a8
ysr <141933879+jysr0@users.noreply.github.com>
2026-02-05 17:39:16 +0100

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -4,17 +4,25 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 
+// vanitygaps
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
+// bar
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+
+// allowkillrule
 static const int allowkill          = 1;        /* allow killing clients by default? */
+
+// fonts
 static const char *fonts[]          = { "CaskaydiaMono NFM:size=12", "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "CaskaydiaMono NFM:size=12";
+
+// colors
 static const char col_gray1[]       = "#191919";
 static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#555555";
@@ -28,13 +36,15 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray5, /*col_gray1*/ col_cyan, /*col_redb*/ col_cyan },
 };
 
+/* scratchpads */
 // specify an app as scratchpad in scratchpads[], then optionaly specify a rule for it in rules[], then bind it to a keybind in keys[].
 // if a scratchpad wanted to be removed just remove its keybind.
 typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+//const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd1[] = {"st", "-n", "spterm", NULL };
 const char *spcmd2[] = {"alacritty", "--class", "lf", "-e", "lfub", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
@@ -55,7 +65,7 @@ static const Rule rules[] = {
 	{ "Firefox",             NULL,             NULL,       0,            0,           0,           -1 },
 	{ "mpv",                 NULL,             NULL,       0,            1,           1,           -1 },
 	{ "Nsxiv",               NULL,             NULL,       0,            1,           1,           -1 },
-	{ NULL,                  "spterm",         NULL,       SPTAG(0),     0,           1,           -1 },
+	{ NULL,                  "spterm",         NULL,       SPTAG(0),     0,           0,           -1 },
 	{ NULL,                  "lf",             NULL,       SPTAG(1),     0,           0,           -1 },
 };
 
@@ -108,24 +118,38 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "rofi", "-show", "drun" };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-i", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_cyan, "-sf", col_gray5, NULL };
 static const char *termcmd[]  = { "st", NULL };
+
 #include "shift-tools-scratchpads.c"
 #include "bulkill.c"
-#include "movestack.c"
+
 static const Key keys[] = {
     /* modifier                                     key                       function                       argument */
+    
+    // spawn programs
     { MODKEY,                                       XK_r,                     spawn,                         {.v = dmenucmd } },
     { MODKEY,                                       XK_Return,                spawn,                         {.v = termcmd } },
+    
+    // toggle bar on/off
     { MODKEY,                                       XK_b,                     togglebar,                     {0} },
    
+    // sticky
+    { MODKEY|Mod1Mask,                              XK_s,                     togglesticky,                  {0} },
+   
+    // tag current client to all tags
+    { MODKEY|ShiftMask,                             XK_0,                     tag,                           {.ui = ~0 } },
+    
+    // view all tags (including scratchpads)
+    { MODKEY,                                       XK_0,                     view,                          {.ui = ~0 } },
+
+    // scratchpads
     { MODKEY,            			    XK_z,  	              togglescratch,                 {.ui = 0 } },
     //{ MODKEY|ShiftMask,            		    XK_e,	              togglescratch,                 {.ui = 1 } },
 
-    { MODKEY|Mod1Mask,                              XK_s,                     togglesticky,                  {0} },
-    
+    // shift tools
     { Mod1Mask,                                     XK_o,                     shiftviewclients,              { .i = +1 } },
     { Mod1Mask,	                                    XK_i,                     shiftviewclients,              { .i = -1 } },
     { Mod1Mask|ShiftMask,                           XK_o,                     shiftview,                     { .i = +1 } },
-    { Mod1Mask|ShiftMask,                           XK_i,	                  shiftview,                     { .i = -1 } },
+    { Mod1Mask|ShiftMask,                           XK_i,	              shiftview,                     { .i = -1 } },
     { Mod1Mask,	                                    XK_l,                     shifttag,                      { .i = +1 } },
     { Mod1Mask,	                                    XK_k,                     shifttag,                      { .i = -1 } },
     { Mod1Mask|ShiftMask,	                    XK_l,                     shifttagclients,               { .i = +1 } },
@@ -135,28 +159,41 @@ static const Key keys[] = {
     { Mod1Mask|ShiftMask,	                    XK_period,                shiftswaptags,                 { .i = +1 } },
     { Mod1Mask|ShiftMask,	                    XK_comma,                 shiftswaptags,                 { .i = -1 } },
     
+    // allowkillrule
     { MODKEY,                                       XK_q,                     toggleallowkill,               {0} },
     
-    { MODKEY|ControlMask,                           XK_j,                     movestack,                     {.i = +1 } },
-    { MODKEY|ControlMask,                           XK_k,                     movestack,                     {.i = -1 } },
-    
+    // push
+    { MODKEY|ControlMask,                           XK_j,                     pushdown,                      {0} },
+    { MODKEY|ControlMask,                           XK_k,                     pushup,                        {0} },
+
+    // switchcol
     { MODKEY,                                       XK_n,                     switchcol,                     {0} },
     
+    // inplacerotate
     { MODKEY|ControlMask|ShiftMask,                 XK_j,                     inplacerotate,                 {.i = +1} },
     { MODKEY|ControlMask|ShiftMask,                 XK_k,                     inplacerotate,                 {.i = -1} },
     { MODKEY|ControlMask|ShiftMask,                 XK_h,                     inplacerotate,                 {.i = +2} },
     { MODKEY|ControlMask|ShiftMask,                 XK_l,                     inplacerotate,                 {.i = -2} },
     
-    { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-    { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+    // change focus within clients
+    { MODKEY,                                       XK_j,                     focusstack,                    {.i = +1 } },
+    { MODKEY,                                       XK_k,                     focusstack,                    {.i = -1 } },
 
+    // increment number of masters
     { MODKEY,                                       XK_i,                     incnmaster,                    {.i = +1 } },
     { MODKEY,                                       XK_d,                     incnmaster,                    {.i = -1 } },
+    
+    // reset number of masters
     { MODKEY,                                       XK_o,                     resetnmaster,                  {0} },
+    
+    // change size of master client
     { MODKEY,                                       XK_h,                     setmfact,                      {.f = -0.05} },
     { MODKEY,                                       XK_l,                     setmfact,                      {.f = +0.05} },
+    
+    // zoomswap
     { MODKEY|Mod1Mask,                              XK_Return,                zoom,                          {0} },
     
+    // vanitygaps
     { MODKEY|Mod1Mask,                              XK_u,                     incrgaps,                      {.i = +1 } },
     { MODKEY|Mod1Mask|ShiftMask,                    XK_u,                     incrgaps,                      {.i = -1 } },
     { MODKEY|Mod1Mask,                              XK_i,                     incrigaps,                     {.i = +1 } },
@@ -174,11 +211,17 @@ static const Key keys[] = {
     { MODKEY|Mod1Mask,                              XK_0,                     togglegaps,                    {0} },
     { MODKEY|Mod1Mask|ShiftMask,                    XK_0,                     defaultgaps,                   {0} },
     
+    // shift view between current selected client in current tag and last selected client in other tag
     { MODKEY,                                       XK_Tab,                   view,                          {0} },
+    
+    // to kill a client
     { MODKEY|ShiftMask,                             XK_c,                     killclient,                    {0} },
+    
+    // bulkill
     { MODKEY|ControlMask,                           XK_c,                     bulkill,                       {.ui = 1} },  // kill unselect
     { MODKEY|ShiftMask|ControlMask,                 XK_c,                     bulkill,                       {.ui = 2} },  // killall
     
+    // layouts
     { MODKEY,                                       XK_t,                     setlayout,                     {.v = &layouts[0]} }, // tile
     { MODKEY,                                       XK_f,                     setlayout,                     {.v = &layouts[1]} }, // float
     { MODKEY,                                       XK_m,                     setlayout,                     {.v = &layouts[2]} }, // monocle
@@ -191,8 +234,13 @@ static const Key keys[] = {
     { MODKEY,                                       XK_p,                     setlayout,                     {.v = &layouts[12]} }, // centeredmaster
     { MODKEY|ShiftMask,                             XK_p,                     setlayout,                     {.v = &layouts[13]} }, // centeredfloatingmaster
     
+    // toggle between current layout and last used layout
     { MODKEY,                                       XK_space,                 setlayout,                     {0} },
+    
+    // toggle current selected client to float or tiled
     { MODKEY|ShiftMask,                             XK_space,                 togglefloating,                {0} },
+    
+    // moveresize
     { MODKEY,                                       XK_Down,                  moveresize,                    {.v = "0x 25y 0w 0h" } },
     { MODKEY,                                       XK_Up,                    moveresize,                    {.v = "0x -25y 0w 0h" } },
     { MODKEY,                                       XK_Right,                 moveresize,                    {.v = "25x 0y 0w 0h" } },
@@ -201,13 +249,14 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,                             XK_Up,                    moveresize,                    {.v = "0x 0y 0w -25h" } },
     { MODKEY|ShiftMask,                             XK_Right,                 moveresize,                    {.v = "0x 0y 25w 0h" } },
     { MODKEY|ShiftMask,                             XK_Left,                  moveresize,                    {.v = "0x 0y -25w 0h" } },
-    { MODKEY,                                       XK_0,                     view,                          {.ui = ~0 } },
-    { MODKEY|ShiftMask,                             XK_0,                     tag,                           {.ui = ~0 } },
+    
+    // for monitors
     { MODKEY,                                       XK_comma,                 focusmon,                      {.i = -1 } },
     { MODKEY,                                       XK_period,                focusmon,                      {.i = +1 } },
     { MODKEY|ShiftMask,                             XK_comma,                 tagmon,                        {.i = -1 } },
     { MODKEY|ShiftMask,                             XK_period,                tagmon,                        {.i = +1 } },
 
+    // jump between tags
     TAGKEYS(                                        XK_1,                                                     0)
     TAGKEYS(                                        XK_2,                                                     1)
     TAGKEYS(                                        XK_3,                                                     2)
@@ -218,7 +267,8 @@ static const Key keys[] = {
     TAGKEYS(                                        XK_8,                                                     7)
     TAGKEYS(                                        XK_9,                                                     8)
   
-  //{ MODKEY|ShiftMask,             XK_q,      quit,            {0} },
+    // exit dwm
+    //{ MODKEY|ShiftMask,             XK_q,      quit,            {0} },
     { MODKEY|ShiftMask,             XK_q,      spawn,           SHCMD("loginctl -s 15 --no-ask-password terminate-session ${XDG_SESSION_ID}") },
 };
 
@@ -226,20 +276,34 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
     /* click                event mask               button          function               argument */
+  
+    // toggle between current layout and last used layout
     { ClkLtSymbol,          0,                       Button1,        setlayout,             {0} },
-    { ClkLtSymbol,          0,                       Button3,        setlayout,             {.v = &layouts[2]} },
+    
+    // layout
+    { ClkLtSymbol,          0,                       Button3,        setlayout,             {.v = &layouts[2]} }, //monocle
+
+    // zoomswap
     { ClkWinTitle,          0,                       Button2,        zoom,                  {0} },
+    
+    // disabled by zoomswap patch
+    //{ ClkClientWin,         MODKEY,                  Button2,        togglefloating,        {0} },
+
+    // move/resize clients
+    { ClkClientWin,         MODKEY,                  Button1,        movemouse,             {0} },
+    { ClkClientWin,         MODKEY|ShiftMask,        Button1,        resizemouse,           {0} },
+    
+    // launch terminal when clicking on status text at bar
     { ClkStatusText,        0,                       Button2,        spawn,                 {.v = termcmd } },
     
+    // statuscmd
     { ClkStatusText,        0,                       Button1,        sigstatusbar,          {.i = 1} },
     { ClkStatusText,        0,                       Button2,        sigstatusbar,          {.i = 2} },
     { ClkStatusText,        0,                       Button3,        sigstatusbar,          {.i = 3} },
     { ClkStatusText,        0,                       Button4,        sigstatusbar,          {.i = 4} },
     { ClkStatusText,        0,                       Button5,        sigstatusbar,          {.i = 5} },
     
-    { ClkClientWin,         MODKEY,                  Button1,        movemouse,             {0} },
-    { ClkClientWin,         MODKEY,                  Button2,        togglefloating,        {0} },
-    { ClkClientWin,         MODKEY|ShiftMask,        Button1,        resizemouse,           {0} },
+    // tags view 
     { ClkTagBar,            0,                       Button1,        view,                  {0} },
     { ClkTagBar,            0,                       Button3,        toggleview,            {0} },
     { ClkTagBar,            MODKEY,                  Button1,        tag,                   {0} },
