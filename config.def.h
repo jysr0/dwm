55fface5c49e36a179af1f24c5f1adb0e7a89c46
ysr <141933879+jysr0@users.noreply.github.com>
2026-07-21 23:14:36 +0100

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 
 // vanitygaps
@@ -19,7 +19,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int allowkill          = 1;        /* allow killing clients by default? */
 
 // fonts
-static const char *fonts[]          = { "CaskaydiaMono NFM:style=Regular:size=12", "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true" };
+static const char *fonts[]          = { "CaskaydiaMono NFM:style=SemiLight:size=11", "Noto Color Emoji:pixelsize=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:style=Regular:size=10";
 
 // colorschemes
